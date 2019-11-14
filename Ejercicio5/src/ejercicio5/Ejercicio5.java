6a3f00b1e24c8e1319b19952734358a6cb4e4933
Gianela Reyna <54996134+GianelaR@users.noreply.github.com>
2019-11-14 18:38:53 -0500

    Update Ejercicio5.java

--- Ejercicio5/src/ejercicio5/Ejercicio5.java
+++ Ejercicio5/src/ejercicio5/Ejercicio5.java
@@ -16,6 +16,10 @@ public class Ejercicio5 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        int a =3;
+        int b=5;
+        int suma;
     }
     
 }
